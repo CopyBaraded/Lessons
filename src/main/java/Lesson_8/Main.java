84d5840b9ddec270f1fd71d22539223ed09b0c62
Baraded <borodeda@gmail.com>
2025-03-10 22:57:31 +0300

    all done

--- src/main/java/Lesson_8/Main.java
+++ src/main/java/Lesson_8/Main.java
@@ -1,24 +1,34 @@
 package Lesson_8;
+/*
+В методе main() вызвать полученный метод, обработать возможные исключения
+MyArraySizeException и MyArrayDataException и вывести результат расчета.
+
+Напишите код для генерации и поимки ArrayIndexOutOfBoundsException.
+ */
 
 import static Lesson_8.ArrayMaster.checkArray;
 
 public class Main {
     public static void main(String[] args) {
+
+        String[][] array = {
+                {"1", "6", "3", "5"},
+                {"2", "0", "2", "6"},
+                {"4", "6", "a", "7"},
+                {"3", "7", "12", "4"}
+        };
         try {
-            String[][] array = {
-                    {"1", "6", "3", "5"},
-                    {"2", "0", "2", "6"},
-                    {"4", "6", "4", "7"},
-                    {"3", "7", "12", "4"}
-            };
-
-            System.out.println("sum  " + checkArray(array));
-
+            System.out.println("Сумма чисел массива:  " + checkArray(array));
         }
-        catch (MyArraySizeException | MyArrayDataException e) {
+        catch (MyArraySizeException | MyArrayDataException  e) {
             System.err.println(e.getMessage());
         }
-
+        try {
+            System.out.println("Элемент массива:  " + array[10][4]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Ошибка: выход за границы массива  " + e.getMessage() + "\n Программа отработала корректно");
+        }
     }
 }
 
